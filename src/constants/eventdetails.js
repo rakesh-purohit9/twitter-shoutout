9ab98e71a4ca2127daf79e292367ce5434ca14c6
Saloni Saraiya <saloni.saraiya@dhiwise.com>
2022-12-07 15:15:14 +0530

    hashtags removed

--- src/constants/eventdetails.js
+++ src/constants/eventdetails.js
@@ -1,9 +1,9 @@
 export const event = {
     handle: 'AWSPune',
-    hashtags: "#ACDPune2022 #AWSPune2022"
+    hashtags: "#ACDPune2022 #AWSPune2022 #BuildWithDhiWise💻"
 }
 
 // export const event = {
 //     handle: 'GDGPune',
-//     hashtags: "#GDGPune2022 #DevFestPune2022"
+//     hashtags: "#GDGPune2022 #DevFestPune2022 #BuildWithDhiWise💻"
 // }
