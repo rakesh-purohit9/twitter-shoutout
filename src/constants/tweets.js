e0dd615e932b591fc1677adbd7a374ea0e561566
Rakesh Purohit <98805545+rakesh-purohit9@users.noreply.github.com>
2022-12-11 00:17:18 +0530

    capitalize DhiWise

--- src/constants/tweets.js
+++ src/constants/tweets.js
@@ -8,7 +8,7 @@ If you’re here, I recommend you to come here. The team is fantastic and has go
   about: `DhiWise Booth Visit`
 }, {
   id: 2,
-  content: `Today at @${event.handle}, @WTMPune, I stopped by @dhiwise booth, and the crowd is huge here!! I got so much to know and take home. I insist on coming here.`,
+  content: `Today at @${event.handle}, @WTMPune, I stopped by @DhiWise booth, and the crowd is huge here!! I got so much to know and take home. I insist on coming here.`,
   about: 'DhiWise Booth Visit'
 }, {
   id: 3,
@@ -16,41 +16,41 @@ If you’re here, I recommend you to come here. The team is fantastic and has go
   about: 'Took A Demo'
 }, {
   id: 4,
-  content: `@dhiwise converts Figma into application code with API Integration and common components. I am excited to know more about it.  @${event.handle}, @WTMPune.`,
+  content: `@DhiWise converts Figma into application code with API Integration and common components. I am excited to know more about it.  @${event.handle}, @WTMPune.`,
   about: 'Took A Demo'
 }, {
   id: 5,
-  content: `I just played an amazing quiz🤩 at DhiWise booth @${event.handle}, @WTMPune and won this super cool swag! 🥳 Do give it a visit yourself and get one😍 @dhiwise.`,
+  content: `I just played an amazing quiz🤩 at DhiWise booth @${event.handle}, @WTMPune and won this super cool swag! 🥳 Do give it a visit yourself and get one😍 @DhiWise.`,
   about: 'Quiz Winner'
 },
 {
   id: 6,
-  content: `Played a quiz @dhiwise booth @${event.handle}, @WTMPune and won this amazing swag of @dhiwise 😍🤩 `,
+  content: `Played a quiz @DhiWise booth @${event.handle}, @WTMPune and won this amazing swag of @DhiWise 😍🤩 `,
   about: 'Quiz Winner'
 },
 {
   id: 7,
-  content: `Got these amazing swags from @dhiwise booth today at @${event.handle}, @WTMPune`,
+  content: `Got these amazing swags from @DhiWise booth today at @${event.handle}, @WTMPune`,
   about: 'Swags'
 }, {
   id: 8,
-  content: `I just got myself this colorful tattoo😍 at @dhiwise booth @${event.handle}, @WTMPune. Have yours NOW! `,
+  content: `I just got myself this colorful tattoo😍 at @DhiWise booth @${event.handle}, @WTMPune. Have yours NOW! `,
   about: 'DhiWise Tattoo'
 }, {
   id: 9,
-  content: `Having a tattoo @dhiwise booth on my hand, and this is giving me some superpower. Get yours when you pass by.  @${event.handle}, @WTMPune. `,
+  content: `Having a tattoo @DhiWise booth on my hand, and this is giving me some superpower. Get yours when you pass by.  @${event.handle}, @WTMPune. `,
   about: 'DhiWise Tattoo'
 }, {
   id: 10,
-  content: `Look How cool I look in this amazing frame by @dhiwise. Come to @dhiwise booth @${event.handle}, @WTMPune and click cool photos here and show them to your friends 😎.`,
+  content: `Look How cool I look in this amazing frame by @DhiWise. Come to @DhiWise booth @${event.handle}, @WTMPune and click cool photos here and show them to your friends 😎.`,
   about: 'DhiWise Photo Frame'
 }, {
   id: 11,
-  content: `Flaunting with DhiWise Mascot Automaton @dhiwise booth @${event.handle}, @WTMPune. See how stunning we look! 😉 `,
+  content: `Flaunting with DhiWise Mascot Automaton @DhiWise booth @${event.handle}, @WTMPune. See how stunning we look! 😉 `,
   about: 'DhiWise Mascot'
 }, {
   id: 12,
-  content: `DhiWise built usecases are amazing to know if you’re at @${event.handle}, @WTMPune. Visit @dhiwise booth, as they did 30 SaaS product integration with OpenAPIs in 30 Days 💪.`,
+  content: `DhiWise built usecases are amazing to know if you’re at @${event.handle}, @WTMPune. Visit @DhiWise booth, as they did 30 SaaS product integration with OpenAPIs in 30 Days 💪.`,
   about: 'DhiWise Usecases'
 }
 
